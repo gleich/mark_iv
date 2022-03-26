042239461e616fa6a1bd870776c1d5b1f820df06
Matt Gleich <git@mattglei.ch>
2022-03-26 13:22:48 -0400

    feat: status LED
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
-
 use cortex_m_rt::entry;
-
 use defmt::info;
-use embedded_graphics::image::{Image, ImageRaw};
-use embedded_graphics::pixelcolor::Rgb565;
-use embedded_time::rate::Extensions;
-use embedded_time::{duration::*, Clock};
-
-use embedded_hal::timer::CountDown;
-
-use panic_probe as _;
-
-use rp_pico::hal::pac;
-
-use rp_pico::hal;
-
 use defmt_rtt as _;
-
+use embedded_graphics::image::Image;
+use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
-
+use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::timer::CountDown;
+use embedded_time::duration::*;
+use embedded_time::rate::Extensions;
+use panic_probe as _;
+use rp_pico::hal;
+use rp_pico::hal::pac;
 use ssd1306::{prelude::*, Ssd1306};
 use tinybmp::Bmp;
 
@@ -46,7 +37,6 @@ fn main() -> ! {
 
     let sio = hal::Sio::new(pac.SIO);
 
-    // Set the pins up according to their function on this particular board
     let pins = rp_pico::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -54,13 +44,9 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Configure two pins as being I²C, not GPIO
     let sda_pin = pins.gpio16.into_mode::<hal::gpio::FunctionI2C>();
     let scl_pin = pins.gpio17.into_mode::<hal::gpio::FunctionI2C>();
 
-    // Create the I²C driver, using the two pre-configured pins. This will fail
-    // at compile time if the pins are in the wrong mode, or if this I²C
-    // peripheral isn't available on these pins!
     let i2c = hal::I2C::i2c0(
         pac.I2C0,
         sda_pin,
@@ -70,10 +56,8 @@ fn main() -> ! {
         clocks.peripheral_clock,
     );
 
-    // Create the I²C display interface:
     let interface = ssd1306::I2CDisplayInterface::new(i2c);
 
-    // Create a driver instance and initialize:
     let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
     display.init().unwrap();
@@ -82,6 +66,11 @@ fn main() -> ! {
     let mut delay = timer.count_down();
 
     let bmp = Bmp::from_slice(include_bytes!("./logo.bmp")).expect("Failed to load BMP image");
+
+    pins.led
+        .into_push_pull_output()
+        .set_high()
+        .expect("Failed to set board LED to high");
 
     let mut y = 0;
     let mut up = false;
